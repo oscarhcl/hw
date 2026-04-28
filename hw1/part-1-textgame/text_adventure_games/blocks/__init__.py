be155b5d67e296a948ebe8b48b77c2e97def3126
Oscar Hung <108368382+oscarhcl@users.noreply.github.com>
2026-04-28 14:54:20 -0700

    Delete hw1 directory

--- hw1/part-1-textgame/text_adventure_games/blocks/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .base import Block
-from .doors import Locked_Door, Guard_Block, Darkness, Door_Block
-
-__all__ = [Block, Locked_Door, Guard_Block, Darkness, Door_Block]
--- hw1/part-1-textgame/text_adventure_games/things/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import Thing
-from .characters import Character
-from .locations import Location
-from .items import Item
-
-__all__ = [Thing, Character, Location, Item]
